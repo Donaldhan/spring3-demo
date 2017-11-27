dbce1127ad1a36147542bced70ed821a3c3ffa5c
doanldhan <shaoqinghanah@qq.com>
2017-11-27 08:59:27 +0800

    repair test resource and java dir

--- src/main/java/cn/home/modules/context/package-info.java
+++ src/main/java/cn/home/modules/context/package-info.java
@@ -23,6 +23,12 @@
  * ContextClosedEvent=====|GenericApplicationContext 
  * 
  * RequestHandledEvent:DispatcherServlet
+ * =============================
+ * Resource 
+ * ResourceLoader
+ * ResourceLoaderAware
+ * PropertyEditor 
+ * 
  * @author donald
  * 2017年11月23日
  * 上午11:29:41
---
+++ src/test/java/cn/home/config/modules/context/TestResourceLoaderAware.java
@@ -0,0 +1,5 @@
+package cn.home.config.modules.context;
+
+public class TestResourceLoaderAware {
+
+}
